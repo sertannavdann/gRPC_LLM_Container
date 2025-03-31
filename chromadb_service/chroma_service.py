b9c1d6623eb2de2252b7689bd635baf54d5e8f49
Sertan Avdan <sertannavdann>
2025-03-31 03:15:54 -0400

    bug-fixes

--- chromadb_service/chroma_service.py
+++ chromadb_service/chroma_service.py
@@ -1,18 +1,21 @@
 import grpc
 from concurrent import futures
 import chromadb
-from chromadb.config import Settings
 from sentence_transformers import SentenceTransformer
 import chroma_pb2
 import chroma_pb2_grpc
+from grpc_reflection.v1alpha import reflection
 
-# Initialize the embedding model
+
+# Initialize the embedding model.
 embedder = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-# Update the embedding function signature as required
-def embedding_function(input):
-    return embedder.encode(input).tolist()
+class MyEmbeddingFunction:
+    def __call__(self, input):
+        # 'input' is expected to be a list of strings.
+        return embedder.encode(input).tolist()
 
+embedding_function = MyEmbeddingFunction()
 client = chromadb.PersistentClient(path="/app/data")
 collection = client.get_or_create_collection(
     name="documents",
@@ -39,6 +42,15 @@ class ChromaServiceServicer(chroma_pb2_grpc.ChromaServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     chroma_pb2_grpc.add_ChromaServiceServicer_to_server(ChromaServiceServicer(), server)
+
+    # Enable reflection for ChromaService
+    service_names = (
+        chroma_pb2.DESCRIPTOR.services_by_name['ChromaService'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(service_names, server)
+    
+
     server.add_insecure_port("[::]:50052")
     server.start()
     server.wait_for_termination()
--- tool_service/tool_service.py
+++ tool_service/tool_service.py
@@ -4,6 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 import tool_pb2
 import tool_pb2_grpc
+from grpc_reflection.v1alpha import reflection
+
 
 class ToolServiceServicer(tool_pb2_grpc.ToolServiceServicer):
     def CallTool(self, request, context):
@@ -31,6 +33,14 @@ class ToolServiceServicer(tool_pb2_grpc.ToolServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     tool_pb2_grpc.add_ToolServiceServicer_to_server(ToolServiceServicer(), server)
+
+        # Enable reflection for ToolService
+    service_names = (
+        tool_pb2.DESCRIPTOR.services_by_name['ToolService'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(service_names, server)
+    
     server.add_insecure_port("[::]:50053")
     server.start()
     server.wait_for_termination()
